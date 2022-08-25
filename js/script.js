87d96e968d1afa9d00f39656999555af83012647
Nali1919 <my@email.com>
2022-08-25 16:40:14 +0200

    Concludo esercizio

--- js/script.js
+++ js/script.js
@@ -2,8 +2,23 @@ console.log('JS OK!');
 
 
 for (let y = 1; y <= 100; y++) {
-  
+    const nonDivsibile= y % 3!== 0 && y % 5 !=0
+    const divisibilePerTre = y % 3 === 0 & y % 5 !==0
+    const divisibilePerCinque = y % 5 === 0 & y % 3 !== 0
+    const divisibilePerEntrambi= y % 3 ===0 & y % 5 ===0
     
-    console.log(y);
+    if(divisibilePerTre){
+        console.log(y+'fizz' );
+    }
+
+    if (divisibilePerCinque){
+        console.log( y+'buzz');
+    }
+  if (divisibilePerEntrambi) {
+    console.log( y+'fizzbuzz')
+    }
+    if (nonDivsibile) {
+    console.log(y)
+    }
 
 }
